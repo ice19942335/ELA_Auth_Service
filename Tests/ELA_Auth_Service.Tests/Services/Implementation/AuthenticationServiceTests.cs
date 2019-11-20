a9dfe19f8920e4ec88d5100943a4a96a91cb2571
ice19942335 <ice19942335@gmail.com>
2019-11-20 15:37:19 +0000

    Removed no neede paths

---
+++ Tests/ELA_Auth_Service.Tests/Services/Implementation/AuthenticationServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+
+namespace ELA_Auth_Service.UnitTests.Services.Implementation
+{
+    [TestClass]
+    public class AuthenticationServiceTests
+    {
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+    }
+}
